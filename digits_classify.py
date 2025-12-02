77221714cdc9cea1fc1675f307fad3a83f2f3e5b
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-12-02 11:41:36 +0200

    Also added digits_classify.py

---
+++ digits_classify.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import numpy as np
+import pickle
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from numpy.typing import NDArray
+from pandas import DataFrame
+
+# IMPORTANT: add NN_classifier to path
+sys.path.append("NN_classifier")
+
+# Import neural network implementation
+from neural_network import NeuralNetworkClassifier
+from NN_preprocessing import NN_preprocess_samples, load_samples
+
+# Define default model
+DEFAULT_MODEL = os.path.join("trained_models", "NN_classifier_d5_p1186826_e2000_98_20251202_0112_deeplearning.pkl")
+
+# Define function for classifying 3D digits
+def digits_classify(test_data: NDArray|list[NDArray|DataFrame], model_path: str=DEFAULT_MODEL) -> NDArray: 
+    
+    # Load trained model
+    try: 
+        with open(model_path, "rb") as f: 
+            model = pickle.load(f)
+    except FileNotFoundError: 
+        print(f"Could not load model from {model_path}")
+        return None
+    
+    # Choose preprocessing pipeline according to model type
+    if isinstance(model, NeuralNetworkClassifier): 
+        preprocess_samples = NN_preprocess_samples
+    else: 
+        raise NotImplementedError
+    
+    # Preprocess samples
+    preprocessed_data = preprocess_samples(test_data)
+
+    # Classify preprocessed samples
+    predicted_classes = model.predict(preprocessed_data)
+
+    # Return predicted classes
+    return predicted_classes
+
+# Test classification function
+if __name__ == "__main__": 
+
+    # Load the data
+    samples, labels = load_samples("training_data")
+    labels = np.array(labels)
+
+    # Extract a random test set
+    _, test_data, _, test_labels = train_test_split(samples, labels, test_size=0.2, stratify=labels)
+
+    # Predict the samples
+    predictions = digits_classify(test_data)
+
+    # Evaluate prediction accuracy
+    print(f"Prediction accuracy: {accuracy_score(predictions, test_labels)*100:.1f} %")
+    print(classification_report(predictions, test_labels))
