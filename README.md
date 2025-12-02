5dfb72e0ab798aed16941795803e7e675e96f9d0
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-12-02 16:54:19 +0200

    More cleaning up

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # PRML_project
+
 Three methods to approach the digit classification problem
+
+## MAIN IMPLEMENTATION FILES
+
+- `digits_classify.ipynb`: the main function implementation in notebook format
+
+- `digits_classify.py`: same implementation as in `digits_classify.ipynb`, but as a runnable script. Function `digits_classify` can also be imported to another file from here.
+
+- `requirements.txt`: project requirements
+
+## Other files
+
+- `NN_classifier.ipynb`: neural network classifier initialization, training, testing and performance evaluation
+
+- `RF_training.ipynb`: random forest model initialization and training
+
+- `RF_for_testing.ipynb`: random forest testing and performance evaluation
+
+- `run_svm.py`: SVM model initialization, training, testing and performance evaluation
+
+## Subdirectories
+
+- `training_data`: contains the training data files
+
+- `trained_models`: contains trained model files, which are utilized by function `classify_digits`
+
+- `NN_classifier`: neural network classifier implementation code and some results
+
+- `Random_forest`: random forest implementation code
+
+- `SVM`: SVM model implementation code
 
 ## 1) NEURAL NETWORKS implementation
 The folder "neural_network" includes seven files:
--- digits_classify.py
+++ digits_classify.py
@@ -7,15 +7,20 @@ from sklearn.model_selection import train_test_split
 from numpy.typing import NDArray
 from pandas import DataFrame
 
-# IMPORTANT: add NN_classifier to path
-sys.path.append("NN_classifier")
-
 # Import neural network implementation
-from neural_network import NeuralNetworkClassifier
-from NN_preprocessing import NN_preprocess_samples, load_samples
+from NN_classifier.neural_network import NeuralNetworkClassifier
+from NN_classifier.preprocessing import NN_preprocess_samples as NN_preprocessing, load_samples
+
+# Import random forest implementation
+from Random_forest.RandomForest import RandomForest
+from Random_forest.RF_preprocessing import preprocess_samples as RF_preprocessing
+
+# Import support vector machine implementation
+from SVM.train_data_process import interpolate_data
+from SVM.svm_setup import svm_predict
 
 # Define default model
-DEFAULT_MODEL = os.path.join("trained_models", "NN_classifier_d5_p1186826_e2000_98_20251202_0112_deeplearning.pkl")
+DEFAULT_MODEL = os.path.join("trained_models", "NN_classifier.pkl")
 
 # Define function for classifying 3D digits
 def digits_classify(test_data: NDArray|list[NDArray|DataFrame], model_path: str=DEFAULT_MODEL) -> NDArray: 
@@ -28,17 +33,36 @@ def digits_classify(test_data: NDArray|list[NDArray|DataFrame], model_path: str=
         print(f"Could not load model from {model_path}")
         return None
     
+    # Convert samples to numpy arrays
+    test_data = [s.to_numpy() if isinstance(s, DataFrame) else s for s in test_data]
+    
     # Choose preprocessing pipeline according to model type
     if isinstance(model, NeuralNetworkClassifier): 
-        preprocess_samples = NN_preprocess_samples
+        
+        # Preprocess data
+        preprocessed_data = NN_preprocessing(test_data)
+
+        # Classify preprocessed samples
+        predicted_classes = model.predict(preprocessed_data)
+    
+    elif isinstance(model, RandomForest): 
+
+        # Preprocess data
+        preprocessed_data = RF_preprocessing(test_data)
+
+        # Classify preprocessed samples
+        predicted_classes = model.predict(preprocessed_data)
+
+    elif "svm" in model_path: 
+
+        # Preprocess data
+        preprocessed_data = [interpolate_data(s, 55).flatten() for s in test_data]
+
+        # Classify preprocessed samples
+        predicted_classes = [svm_predict(s, model_path) for s in preprocessed_data]
+
     else: 
         raise NotImplementedError
-    
-    # Preprocess samples
-    preprocessed_data = preprocess_samples(test_data)
-
-    # Classify preprocessed samples
-    predicted_classes = model.predict(preprocessed_data)
 
     # Return predicted classes
     return predicted_classes
@@ -55,6 +79,8 @@ if __name__ == "__main__":
 
     # Predict the samples
     predictions = digits_classify(test_data)
+    # predictions = digits_classify(test_data, os.path.join("trained_models", "RF_classifier.pkl"))
+    # predictions = digits_classify(test_data, os.path.join("trained_models", "svm_classifier.pkl"))
 
     # Evaluate prediction accuracy
     print(f"Prediction accuracy: {accuracy_score(predictions, test_labels)*100:.1f} %")
