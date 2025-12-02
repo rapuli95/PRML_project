9b1ebf6d3b8e09b81dc8b8304d28be67d0e293ad
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-12-02 12:48:09 +0200

    some work

--- Random_forest/RandomForest/RandomForest.py
+++ Random_forest/RandomForest/RandomForest.py
@@ -1,6 +1,6 @@
 # credit source:
 # https://github.com/AssemblyAI-Community/Machine-Learning-From-Scratch/tree/main/05%20Random%20Forests
-from DecisionTree import DecisionTree
+from Random_forest.RandomForest.DecisionTree import DecisionTree
 import numpy as np
 from collections import Counter
 
